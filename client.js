1ee12fce37ef070f70c31d70c80f9a43568ebfb8
Xavier Shay <xavier@rhnh.net>
2009-11-27 10:49:25 +1100

    Respect options passed into createConnection

--- client.js
+++ client.js
@@ -3,7 +3,10 @@ require.paths.unshift('lib');
 var sys =  require('sys');
 var AMQP = require('amqp');
 
-var conn = AMQP.createConnection({});
+var conn = AMQP.createConnection({
+  host: 'localhost',
+  port: 5672
+});
 
 conn.addListener("connect", function() {
   sys.puts("Connected to AMQP server");
