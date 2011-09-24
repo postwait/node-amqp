8bc3fdaaadca44592da908823d09b13f4de47657
Bernhard K. Weisshuhn <bkw@codingforce.com>
2011-09-24 14:16:35 +0200

    Test for queue arguments, based on work by Dušan Maliarik

---
+++ test/test-queue-args.js
@@ -0,0 +1,48 @@
+require('./harness');
+
+connection.on('ready', function() {
+  var qName = 'node-queue-args-queue';
+  puts("connected to " + connection.serverProperties.product);
+
+  cleanUp(connection, qName, doCheck);
+});
+
+
+function doCheck(conn1, qName) {
+  conn1.queue( qName, {'arguments': {'x-expires': 300}}, function(q1) {
+    puts("queue declared");
+    var conn2 = amqp.createConnection({});
+    conn2.on('ready', function() {
+      var q2 = conn2.queue(
+        qName, {'arguments': {'x-expires': 301}}, function() {
+          puts("second queue declared");
+        }
+      );
+      q2.on('error', function(err) {
+        assert.equal(err.code, 406);
+        assert.ok(err.message.indexOf('PRECONDITION_FAILED') == 0);
+        conn1.end();
+        conn2.end();
+      });
+      q2.on('queueDeclareOk', function(event) {
+        assert.ok(false, 'queue should not have been created');
+      });
+    });
+  });
+};
+
+function cleanUp(conn, qName, actualTest) {
+  // start with a clean slate: make sure queue doesn't exist
+  q = connection.queue(qName, {passive: true}, function (qName) {
+    q.destroy();
+    q.on('queueDeleteOk', function() { return actualTest(connection, qName) });
+  });
+
+  q.on('error', function (err) {
+    if (err.code == 404) {
+      // ok, queue doesn't exist
+      return actualTest(connection, qName);
+    }
+    throw err;
+  });
+}
