b6d934abeb69c8e930836b9f251fe30d36a8c99c
Tobias Ullrich <tobias@bluekai.com>
2012-04-16 16:53:36 +0200

    making sure that sourceEnd for data.copy does not exceed data.length

--- amqp.js
+++ amqp.js
@@ -166,7 +166,8 @@ function AMQPParser (version, type) {
   function frame(data) {
     var fb = frameBuffer;
     var needed = fb.length - fb.used;
-    data.copy(fb, fb.used, 0, data.length);
+    var sourceEnd = (fb.length > data.length) ? data.length : fb.length;
+    data.copy(fb, fb.used, 0, sourceEnd);
     fb.used += data.length;
     if (data.length > needed) {
       return frameEnd(data.slice(needed));
