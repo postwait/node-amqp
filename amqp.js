eb81a49eaf5413a0af47f0941e0a44c4ec83a98c
ssafejava <s@safejava.com>
2013-08-23 10:23:14 +0800

    Simplify amqp export

--- amqp.js
+++ amqp.js
@@ -1,12 +1,13 @@
 'use strict';
 var Connection = require('./lib/connection');
     
-module.exports = {Connection: Connection};
-
-module.exports.createConnection = function (options, implOptions, readyCallback) {
-  var c = new Connection(options, implOptions, readyCallback);
-  // c.setOptions(options);
-  // c.setImplOptions(implOptions);
-  c._connect();
-  return c;
+module.exports = {
+  Connection: Connection,
+  createConnection: function (options, implOptions, readyCallback) {
+    var c = new Connection(options, implOptions, readyCallback);
+    // c.setOptions(options);
+    // c.setImplOptions(implOptions);
+    c._connect();
+    return c;
+  }
 };
